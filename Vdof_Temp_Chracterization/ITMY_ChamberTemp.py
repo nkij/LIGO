53d20dca093d8472ab98b9cfe2716aa31012cfb5
Nutsinee Kijbunchoo <nutsinee.kijbunchoo@ligo.org>
2014-12-15 13:41:02 -0600

    edited ITMY_ChamberTemp.py

--- Vdof_Temp_Chracterization/ITMY_ChamberTemp.py
+++ Vdof_Temp_Chracterization/ITMY_ChamberTemp.py
@@ -8,17 +8,19 @@ end = Time('2014-12-01 21:00:00', format='iso', scale='utc')
 print start.iso, start.gps
 print end.iso, end.gps
 
-TCS = TimeSeries.fetch('L1:TCS-ITMY_HWS_CHAMBERTEMPERATURESENSORB.mean,m-trend', start, end, verbose=True)
+#TCS = TimeSeries.fetch('L1:TCS-ITMY_HWS_CHAMBERTEMPERATURESENSORB.mean,m-trend', start, end, verbose=True)
+RH = TimeSeries.fetch('L1:TCS-ITMY_RH_LOWERRTD.mean,m-trend', start, end, verbose=True)
 ITMY = TimeSeries.fetch('L1:SUS-ITMY_M0_DAMP_V_IN1_DQ.mean,m-trend', start, end, verbose=True)
 
 print 'data fetched'
 
-plot = TimeSeriesPlot(ITMY, TCS, sep=True)
+plot = TimeSeriesPlot(ITMY, RH, sep=True)
 
 ax1 = plot.axes[0]
 ax1.set_ylim(-90,-55)
 ax1.set_ylabel('um')
-ax1.set_title('Temperature vs. ITMY\_V (4 days)')
+ax1.lines[0].set_color('red')
+ax1.set_title('(RH) Temperature vs. ITMY\_V (4 days)')
 ax1.legend(loc='upper right',  ncol=1, fancybox=True, shadow=True) 
 ax1.grid(True, which='both', axis='both') # add more grid   
 #mj  = MultipleLocator(2)
@@ -27,7 +29,7 @@ ml1 = MultipleLocator(1)
 ax1.yaxis.set_minor_locator(ml1)
 
 ax2 = plot.axes[1]
-ax2.set_ylim(18,18.5)
+ax2.set_ylim(18.4,19.4)
 ax2.set_ylabel('C')
 ax2.legend(loc='upper right',  ncol=1, fancybox=True, shadow=True)
 ax2.grid(True, which='both', axis='both') # add more grid
